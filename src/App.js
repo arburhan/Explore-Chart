4160e6275cd4e74ee92821431d4a712cdfb4e0f0
arburhan <buthan190629@outlook.com>
2022-06-06 18:21:38 +0600

    install package

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select className="select select-bordered w-md max-w-xs">
+        <option selected>Weekly</option>
+        <option>Monthly</option>
+        <option>Yearly</option>
+      </select>
     </div>
   );
 }
