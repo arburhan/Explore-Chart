0385a2467d16683a8d71bf24843451fd629b235d
arburhan <buthan190629@outlook.com>
2022-06-08 21:51:43 +0600

    add usequery

--- src/App.js
+++ src/App.js
@@ -1,204 +1,29 @@
 import './App.css';
 import { Area, Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { useState } from 'react';
+import { useQuery } from 'react-query';
 
 function App() {
   const [dataState, setDataState] = useState([]);
-
-  const weekData = [
-    {
-      name: 'Saturday',
-      Income: 590,
-      Expenses: 800,
-      amt: 1400,
-    },
-    {
-      name: 'Sunday',
-      Income: 868,
-      Expenses: 967,
-      amt: 1506,
-    },
-    {
-      name: 'Monday',
-      Income: 2000,
-      Expenses: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Tuesday',
-      Income: 2780,
-      Expenses: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Wednesday',
-      Income: 1890,
-      Expenses: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Thursday',
-      Income: 2390,
-      Expenses: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Friday',
-      Income: 3490,
-      Expenses: 4300,
-      amt: 2100,
-    },
-  ];
-  const monthData = [
-    {
-      name: 'January',
-      Income: 590,
-      Expenses: 800,
-      amt: 1400,
-    },
-    {
-      name: 'February',
-      Income: 868,
-      Expenses: 967,
-      amt: 1506,
-    },
-    {
-      name: 'March',
-      Income: 2000,
-      Expenses: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'April',
-      Income: 2780,
-      Expenses: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'May',
-      Income: 1890,
-      Expenses: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'June',
-      Income: 2390,
-      Expenses: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'July',
-      Income: 3490,
-      Expenses: 4300,
-      amt: 2100,
-    },
-    {
-      name: 'August',
-      Income: 2000,
-      Expenses: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'September',
-      Income: 2780,
-      Expenses: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'October',
-      Income: 1890,
-      Expenses: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'November',
-      Income: 2390,
-      Expenses: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'December',
-      Income: 3490,
-      Expenses: 4300,
-      amt: 2100,
-    },
-  ];
-  const yearData = [
-    {
-      name: 2011,
-      Income: 590,
-      Expenses: 800,
-      amt: 1400,
-    },
-    {
-      name: 2012,
-      Income: 868,
-      Expenses: 967,
-      amt: 1506,
-    },
-    {
-      name: 2013,
-      Income: 2000,
-      Expenses: 9800,
-      amt: 2290,
-    },
-    {
-      name: 2014,
-      Income: 2780,
-      Expenses: 3908,
-      amt: 2000,
-    },
-    {
-      name: 2015,
-      Income: 1890,
-      Expenses: 4800,
-      amt: 2181,
-    },
-    {
-      name: 2016,
-      Income: 2390,
-      Expenses: 3800,
-      amt: 2500,
-    },
-    {
-      name: 2017,
-      Income: 3490,
-      Expenses: 4300,
-      amt: 2100,
-    },
-    {
-      name: 2018,
-      Income: 2000,
-      Expenses: 9800,
-      amt: 2290,
-    },
-    {
-      name: 2019,
-      Income: 2780,
-      Expenses: 3908,
-      amt: 2000,
-    },
-    {
-      name: 2020,
-      Income: 1890,
-      Expenses: 4800,
-      amt: 2181,
-    },
-    {
-      name: 2021,
-      Income: 2390,
-      Expenses: 3800,
-      amt: 2500,
-    },
-    {
-      name: 2022,
-      Income: 3490,
-      Expenses: 4300,
-      amt: 2100,
-    },
-  ];
   const [trackCharts, setTrackCharts] = useState('');
-  console.log(trackCharts)
+
+  // week data
+  const { data: weekData, isLoading: weekLoading } = useQuery(['weekData'], () => fetch(`Weekly.json`)
+    .then(res => res.json()));
+
+  // monthly data
+  const { data: monthData, isLoading: monthLoading } = useQuery(['monthData'], () => fetch(`Monthly.json`)
+    .then(res => res.json()));
+
+  // monthly data
+  const { data: yearData, isLoading: yearLoading } = useQuery(['yearData'], () => fetch(`Yearly.json`)
+    .then(res => res.json()));
+
+  // loading system 
+  if (weekLoading || monthLoading || yearLoading) {
+    return <p>Loading ...</p>
+  }
+  // function
   const trackChart = (e) => {
     setTrackCharts(e.target.value);
   }
